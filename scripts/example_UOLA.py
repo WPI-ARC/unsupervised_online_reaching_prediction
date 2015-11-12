cfd0e72675cb6fbee3a1d80bd5dc4d1a702fb354
ruikunl <luoruikun1989@gmail.com>
2015-11-12 10:05:27 -0500

    change example UOLA, and some prints

--- scripts/example_UOLA.py
+++ scripts/example_UOLA.py
@@ -11,14 +11,22 @@ if __name__ == '__main__':
     testTrajCsvFile = baseDir + "csvFiles/testTraj.csv"
     modelDir = baseDir + "TRO_model.mat"
 # Initialize Matlab
+    print 'Loading MATLAB.'
     session = pymatlab.session_factory()
+    print 'Finish loading MATLAB.'
     session.run('cd ' + baseDir)
 # Initialize Model
+    print 'Start Initializing the model.'
     session.run('UOLA_init(\''+modelDir+'\')')
+    print 'Finish Initializing the model'
 # Model Update
+    print 'Start Updating the model using one trajectory'
     session.run('UOLA_learn(\'' + modelDir + '\',\'' + obsTrajCsvFile + '\')')
+    print 'Finish Updating the model using one trajectory'
 # Prediction
+    print 'Start Predicting the given trajectory.'
     session.run('UOLA_predict(\'' + modelDir + '\',\'' + testTrajCsvFile + '\',\'' + predTrajCsvFile +'\')' )
+    print 'Finish Predicting the given trajectory.'
     print 'End Example'
 
 
